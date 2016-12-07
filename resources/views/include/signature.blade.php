2a89d0556bff1be0c87199afdee65f5f7231659d
Sajinie <=>
2016-12-08 04:24:59 +0530

    add a include directory to resources/views add 3 blade files to it

---
+++ resources/views/include/signature.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <div class="page-header" style="width: 90%; float:left">
+        <h1>Sarigama Music Academy</h1>
+    </div>
+    <div style="width: 10%; float:right">
+        <img src="http://www.w3schools.com/images/w3schools_green.jpg" alt="W3Schools.com" style="width:104px;height:142px;">
+
+    </div>
+
+</header>
