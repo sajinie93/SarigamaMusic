f3986fd2fe90777a997aad36953a7960f07e6547
Sajinie <=>
2016-12-08 18:04:06 +0530

    add sHome.blade.php to include directory to keep the side bars

---
+++ resources/views/include/sHome.blade.php
@@ -0,0 +1,41 @@
+<header>
+    <div class="col-sm-2 sidenav">
+        <h4>Navigation</h4>
+        <ul class="nav nav-pills nav-stacked">
+            <li class="active"><a href="#section1">Home</a></li>
+            <li><a href="#section3">My Home</a></li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">My Profile <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">View profile</a></li>
+                    <li><a href="#">View progress</a></li>
+
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">My Courses <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Course-1</a></li>
+                    <li><a href="#">Course-2</a></li>
+                    <li><a href="#">Course-3</a></li>
+                </ul>
+            </li>
+
+        </ul><br>
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search..">
+            <span class="input-group-btn">
+          <button class="btn btn-default" type="button">
+            <span class="glyphicon glyphicon-search"></span>
+          </button>
+        </span>
+        </div>
+    </div>
+    <div class="col-sm-8 text-left">
+        <h1>Sample Code</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <hr>
+        <h3>Test</h3>
+        <p>Lorem ipsum...</p>
+    </div>
+</header>
