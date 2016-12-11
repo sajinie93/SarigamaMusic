f1d19706fe34ea88f525b0dbb40ed72052014ad9
Amali Lakshika <maamalilakshika@gmail.com>
2016-12-11 13:24:21 +0530

    add classroom

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,152 +5,48 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">--}}
+    {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>--}}
+    {{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>--}}
+
+   {{--@yield('style')--}}
 
 
-    @yield('style')
+
+    {{--<style>--}}
 
 
+        {{--/* Set height of the grid so .sidenav can be 100% (adjust if needed) */--}}
+        {{--.row.content {height: 1500px}--}}
 
-    <style>
+        {{--/* Set gray background color and 100% height */--}}
+        {{--.sidenav {--}}
+            {{--background-color: #f1f1f1;--}}
+            {{--height: 100%;--}}
+        {{--}--}}
 
+        {{--/* Set black background color, white text and some padding */--}}
+        {{--footer {--}}
+            {{--background-color: #555;--}}
+            {{--color: white;--}}
+            {{--padding: 15px;--}}
+        {{--}--}}
 
-        /* Set height of the grid so .sidenav can be 100% (adjust if needed) */
-        .row.content {height: 1500px}
-
-        /* Set gray background color and 100% height */
-        .sidenav {
-            background-color: #f1f1f1;
-            height: 100%;
-        }
-
-        /* Set black background color, white text and some padding */
-        footer {
-            background-color: #555;
-            color: white;
-            padding: 15px;
-        }
-
-        /* On small screens, set height to 'auto' for sidenav and grid */
-        @media screen and (max-width: 767px) {
-            .sidenav {
-                height: auto;
-                padding: 15px;
-            }
-            .row.content {height: auto;}
-        }
-    </style>
+        {{--/* On small screens, set height to 'auto' for sidenav and grid */--}}
+        {{--@media screen and (max-width: 767px) {--}}
+            {{--.sidenav {--}}
+                {{--height: auto;--}}
+                {{--padding: 15px;--}}
+            {{--}--}}
+            {{--.row.content {height: auto;}--}}
+        {{--}--}}
+    {{--</style>--}}
 </head>
 <body>
 
-
-
-{{--<div class="container-fluid">--}}
-{{--<div class="row content">--}}
-{{--<div class="col-sm-2 sidenav">--}}
-{{--<h4>SaRigaMa MusIc</h4>--}}
-{{--<ul class="nav nav-pills nav-stacked">--}}
-{{--<li class="active"><a href="#section1">Dashboard</a></li>--}}
-{{--<li><a href="#section2">Course</a></li>--}}
-{{--<li><a href="#section2">Add Payment</a></li>--}}
-{{--<li><a href="#section2">Payment history</a></li>--}}
-{{--<li><a href="#section2">Teacher salary</a></li>--}}
-{{--<li><a href="#section2">Teacher Salary's History</a></li>--}}
-{{--<li><a href="#section3">Add Student</a></li>--}}
-{{--<li><a href="#section3">All Student</a></li>--}}
-{{--<li><a href="#section3">Leave Student</a></li>--}}
-{{--<li><a href="#section3">Teacher</a></li>--}}
-{{--<li><a href="#section3">Add Teacher</a></li>--}}
-{{--<li><a href="#section3">Leave Teacher</a></li>--}}
-{{--<li><a href="#section3">Assignment Summary</a></li>--}}
-{{--<li><a href="#section3">Student Attendance</a></li>--}}
-{{--<li><a href="#section2">Student Attendance History</a></li>--}}
-{{--<li><a href="#section2">Teacher Attendance</a></li>--}}
-{{--<li><a href="#section2">Teacher Attendance History</a></li>--}}
-{{--<li><a href="#section3">Class Room</a></li>--}}
-{{--<li><a href="#section3">Course Routing</a></li>--}}
-{{--<li><a href="#section3">Class Routing</a></li>--}}
-
-
-{{--</ul><br>--}}
-{{--<div class="input-group">--}}
-{{--<input type="text" class="form-control" placeholder="Search Blog..">--}}
-{{--<span class="input-group-btn">--}}
-{{--<button class="btn btn-default" type="button">--}}
-{{--<span class="glyphicon glyphicon-search"></span>--}}
-{{--</button>--}}
-{{--</span>--}}
-{{--</div>--}}
-{{--</div>--}}
-
-{{--<div class="col-sm-9">--}}
-{{--<h4 style="padding: 0.3cm;"><button class="btn btn-default" type="button" style="float: right; ">--}}
-{{--<a href="#"><span class="glyphicon glyphicon-user"></span> Logout</a>--}}
-{{--</button></h4>--}}
-{{--<hr>--}}
-{{--<h2>I Love Food</h2>--}}
-{{--<h5><span class="glyphicon glyphicon-time"></span> Post by Jane Dane, Sep 27, 2015.</h5>--}}
-{{--<h5><span class="label label-danger">Food</span> <span class="label label-primary">Ipsum</span></h5><br>--}}
-{{--<p>Food is my passion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>--}}
-{{--<br><br>--}}
-
-{{--<h4><small>RECENT POSTS</small></h4>--}}
-{{--<hr>--}}
-{{--<h2>Officially Blogging</h2>--}}
-{{--<h5><span class="glyphicon glyphicon-time"></span> Post by John Doe, Sep 24, 2015.</h5>--}}
-{{--<h5><span class="label label-success">Lorem</span></h5><br>--}}
-{{--<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>--}}
-{{--<hr>--}}
-
-{{--<h4>Leave a Comment:</h4>--}}
-{{--<form role="form">--}}
-{{--<div class="form-group">--}}
-{{--<textarea class="form-control" rows="3" required></textarea>--}}
-{{--</div>--}}
-{{--<button type="submit" class="btn btn-success">Submit</button>--}}
-{{--</form>--}}
-{{--<br><br>--}}
-
-{{--<p><span class="badge">2</span> Comments:</p><br>--}}
-
-{{--<div class="row">--}}
-{{--<div class="col-sm-2 text-center">--}}
-{{--<img src="bandmember.jpg" class="img-circle" height="65" width="65" alt="Avatar">--}}
-{{--</div>--}}
-{{--<div class="col-sm-10">--}}
-{{--<h4>Anja <small>Sep 29, 2015, 9:12 PM</small></h4>--}}
-{{--<p>Keep up the GREAT work! I am cheering for you!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>--}}
-{{--<br>--}}
-{{--</div>--}}
-{{--<div class="col-sm-2 text-center">--}}
-{{--<img src="bird.jpg" class="img-circle" height="65" width="65" alt="Avatar">--}}
-{{--</div>--}}
-{{--<div class="col-sm-10">--}}
-{{--<h4>John Row <small>Sep 25, 2015, 8:25 PM</small></h4>--}}
-{{--<p>I am so happy for you man! Finally. I am looking forward to read about your trendy life. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>--}}
-{{--<br>--}}
-{{--<p><span class="badge">1</span> Comment:</p><br>--}}
-{{--<div class="row">--}}
-{{--<div class="col-sm-2 text-center">--}}
-{{--<img src="bird.jpg" class="img-circle" height="65" width="65" alt="Avatar">--}}
-{{--</div>--}}
-{{--<div class="col-xs-10">--}}
-{{--<h4>Nested Bro <small>Sep 25, 2015, 8:28 PM</small></h4>--}}
-{{--<p>Me too! WOW!</p>--}}
-{{--<br>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-
 <div class="container-fluid">
     <div class="row content">
-        <div class="col-sm-2 sidenav">
+        <div class="col-sm-2">
             @yield('side-bar')
         </div>
 
@@ -162,7 +58,7 @@
 
 
 <footer class="container-fluid">
-    <p>Footer Text</p>
+    <p>@yield('footer')</p>
 </footer>
 
 </body>
