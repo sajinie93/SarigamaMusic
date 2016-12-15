29bb5ad3a66f0a91f912addd6f286e99313bc84d
William A Nadeeshani <nadeeshanijayarathna93@gmail.com>
2016-12-15 14:28:05 +0530

    added home.php

---
+++ resources/views/home.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sarigama Music School</title>
+        <link rel="stylesheet" href="../css/style.css" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
+    </head>
+    <body>
+        <div class="page">
+            <div class="header">
+                <a href="login.php" id="logo"><img src="../images/logo.png" alt="logo"></a>
+                <ul class="navigation">
+
+                </ul>
+            </div>
+            <div class="body">
+                <div class="home">
+                    <div class="content">
+                        <div class="featured">
+                            <div>
+                                <p>Sarigama Music School</p>
+                                <p>
+                                    For over 25 years SARIGAMA MUSIC SCHOOL has spread the joy of music and performance
+                                </p>
+                            </div>
+
+                        </div>
+                        <div class="section">
+                            <h3>Music Programs</h3>
+                            <ul>
+                                <li>
+                                    <span>Piano</span> <img src="../images/piano.jpg" alt=""></a>
+                                </li>
+                                <li>
+                                    <span>Violin</span><img src="../images/violin.jpg" alt=""></a>
+                                </li>
+                                <li>
+                                    <span>Guitar</span><img src="../images/guitar.jpg" alt=""></a>
+                                </li>
+                                <li>
+                                    <span>Saxophone</span> <img src="../images/saxophone.jpg" alt=""></a>
+                                </li>
+                                <li>
+                                    <span>Drums</span> <img src="../images/drums.jpg" alt=""></a>
+                                </li>
+                                <li>
+                                    <span>Voice Lesson</span> <img src="../images/voice-lesson.jpg" alt=""></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="sidebar">
+                        <ul>
+                            <li>
+                                <span>Login</span>
+                                <div id="formLogin">
+                                    <form action="">
+                                        <div>
+                                            <label for="userName">User ID</label>
+                                            <input type="text" id="userName" style="" name="userName">
+                                            <label for="surname">Password</label>
+                                            <input type="password" id="password" name="password">
+                                            <div class="col m5">
+                                                <p class="right-align"><button id="login" class="btn btn-large waves-effect waves-light midddle" style="background-color:#00838f" type="submit" name="action">Login</button></p>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                <div class="about">
+                    <h3>About</h3>
+                    <div>
+                        <img src="images/instructors.jpg" alt=""></a>
+                        <p>
+                            To educate, develop and inspire students to achieve their full musical potential and provide them the skills for lifelong enjoyment of music.
+                        </p>
+                    </div>
+                </div>
+                <div class="contact">
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>
+                            <span>Location :</span>
+                            <p>
+                                No: 23,
+                                Kandy Road,
+                                Kadawatha.
+                            </p>
+                        </li>
+                        <li>
+                            <span>Email :</span>
+                            <p>
+                                sarigamamusic@gmail.com
+                            </p>
+                        </li>
+                        <li>
+                            <span>Phone :</span>
+                            <p>
+                                (+94) 0112 986 258
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="connect">
+                </div>
+                <p class="footnote">
+                    &#169; Copyright 2016. All rights reserved
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
